2de540c169b2c14c84c5d1e96c6754ebd75bba71
HerrTav <61609035+Haurpa@users.noreply.github.com>
2022-12-19 14:20:15 +0900

    Create Many Flags Elements

---
+++ Many_Flags/Many_Flags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Many_Flags
+//
+//  Created by 이정민 on 2022/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
